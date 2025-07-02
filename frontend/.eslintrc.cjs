15b59336742e13d756b705064ff46550de611809
Victor Gabriel Soares <4vgabriel@gmail.com>
2025-07-02 11:49:04 -0300

    #5 - chore: add ESLint and Prettier configuration files for code quality enforcement

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,66 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime'
+  ],
+  ignorePatterns: [
+    'dist',
+    'build',
+    'node_modules',
+    'coverage',
+    'storybook-static',
+    '*.config.js',
+    '*.config.ts'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint',
+    'react',
+    'react-hooks'
+  ],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { 
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      }
+    ],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'eqeqeq': ['error', 'always'],
+    'curly': ['error', 'all']
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
