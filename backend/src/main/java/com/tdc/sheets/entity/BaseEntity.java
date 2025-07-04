4715e2a9799ee37dd65a0149bdae32cd5b2f4e06
Victor Gabriel Soares <4vgabriel@gmail.com>
2025-07-04 09:09:18 -0300

    #7 - refact: remove createdBy and updatedBy fields from BaseEntity

--- backend/src/main/java/com/tdc/sheets/entity/BaseEntity.java
+++ backend/src/main/java/com/tdc/sheets/entity/BaseEntity.java
@@ -1,9 +1,7 @@
 package com.tdc.sheets.entity;
 
 import jakarta.persistence.*;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -32,14 +30,6 @@ public abstract class BaseEntity {
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
-    @CreatedBy
-    @Column(name = "created_by", updatable = false)
-    private String createdBy;
-
-    @LastModifiedBy
-    @Column(name = "updated_by")
-    private String updatedBy;
-
     @Version
     @Column(name = "version")
     private Integer version;
@@ -67,22 +57,6 @@ public abstract class BaseEntity {
 
     public void setUpdatedAt(LocalDateTime updatedAt) {
         this.updatedAt = updatedAt;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public String getUpdatedBy() {
-        return updatedBy;
-    }
-
-    public void setUpdatedBy(String updatedBy) {
-        this.updatedBy = updatedBy;
     }
 
     public Integer getVersion() {
